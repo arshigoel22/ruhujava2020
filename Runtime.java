10414911776b746271edfaa855340dbfb88aac4d
arshigoel22 <68539256+arshigoel22@users.noreply.github.com>
2020-09-14 11:05:12 +0530

    Add files via upload

---
+++ Runtime.java
@@ -0,0 +1,18 @@
+import java.utill.Scanner;
+class Runtime
+{
+   public static void main(string []args)
+   {
+      int si,p,r,t;
+      Scanner s=new Scanner(System.in);
+      System.out.println("Enter values for p=");
+      p=s.nextlnt();
+      System.out.println("Enter values for r=");
+      r=s.nextInt();
+      System.out.println("Enter values for r=");
+      t=s.nextInt();
+      si=(p*r*t)/100;
+System.out.println("si="+si);
+     }
+}
+       
